1df9b08f3d0e7e6fcb107e4c13414f1cf95c2572
Timur Bikmukhametov <tima.bikmukhametov@inbox.ru>
2024-12-13 23:59:36 +0300

    deploy_7

--- src/main/webapp/js/choose_quiz.js
+++ src/main/webapp/js/choose_quiz.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
               <img src="${quiz.quizIconPath}" alt="${quiz.quizName}" class="quiz-icon">
               <div class="quiz-content"> 
                 <h2 id="title_of_quiz">${quiz.quizName}</h2>
-                <p>${quiz.quizDescription}</p>
+                <h2>${quiz.quizDescription}</h2>
                 <h3 id="quiz_type">${quiz.quizType}</h3>
                 <a href="/game-servlet?id=${quiz.id}" class="quiz-button">Начать квиз</a>
               </div>
