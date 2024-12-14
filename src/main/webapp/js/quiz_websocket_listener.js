dd012b7b8d6580899e0b3998196c2ba463a5279c
Timur Bikmukhametov <tima.bikmukhametov@inbox.ru>
2024-12-14 11:57:46 +0300

    deploy_10

--- src/main/webapp/js/quiz_websocket_listener.js
+++ src/main/webapp/js/quiz_websocket_listener.js
@@ -1,4 +1,5 @@
 //const socket = new WebSocket('ws://localhost:8080/quiz-start-websocket');
+const url = new URL(window.location.href)
 const socket = new WebSocket('ws://'+url.host+'/quiz-start-websocket');
 
 
